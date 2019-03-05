eced3d44c29658d521be04d0c411d8c46660cdc3
ryan m <contact@ryanmeek.com>
2019-03-05 20:28:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def line(array)
+  line_string = "The line is currently"
+  if array.length == 0
+    line_string << " empty."
+  else
+    line_string << ":"
+    array.each{|name| line_string << " #{name.index}. #{name}"}
+  end
+  puts line_string
+end
+
+def take_a_number(array, name)
+  array << name
+  puts "#{array.length}. #{name}"
+end
+
+# def now_serving
+  
